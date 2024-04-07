ce4bbf6f8c424284b8ab8e1c1e4d4d1f0c2eff04
coderkannaujia <kumamit482000@gmail.com>
2024-04-07 19:16:03 +0530

    commit all file

---
+++ src/components/getuser/User.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import "./user.css";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+const User = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:4000/api/getall");
+      setUsers(response.data);
+    };
+    fetchData();
+  }, []);
+
+  
+  const deleteUser = async (userId) => {
+    await axios
+      .delete(`http://localhost:4000/api/deletuser/${userId}`)
+      .then((response) => {
+        setUsers((prevUser) => prevUser.filter((user) => user._id !== userId));
+        toast.success(response.data.
+          message, { position: "top-right" });
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="userTable">
+      <Link to={"/add"} className=" ">
+        Add User
+      </Link>
+      <table border={1} cellPadding={10} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>S.no.</th>
+            <th>User name</th>
+            <th>User Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>
+                  {user.fname} {user.lname}
+                </td>
+                <td>{user.email}</td>
+                <td className="actionButtons">
+                  <button onClick={() => deleteUser(user._id)}>
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                  <Link to={`/edit/` + user._id}>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default User;
